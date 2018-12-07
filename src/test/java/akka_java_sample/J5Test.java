8b99003340086bd70a392e00801241ac863671c4
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-12-07 16:56:29 +0100

    Add JUnit5 support (not for Akka yet)

---
+++ src/test/java/akka_java_sample/J5Test.java
@@ -0,0 +1,12 @@
+package akka_java_sample;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class J5Test {
+  @Test
+  public void tryThis() {
+    assertEquals(1, 1);
+  }
+}
